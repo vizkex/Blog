31e390421ace105916c75539544fc8a4bcad2866
Yousef <117045051+vizkex@users.noreply.github.com>
2023-11-05 22:44:15 +0300

    new repo the first one i f it up

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'vizkex personal Blog';
+export const SITE_DESCRIPTION = 'I write here what i want :P';
+export const HOME_TITLE = 'Home';
+export const BLOG_TITLE = 'BLOG';
